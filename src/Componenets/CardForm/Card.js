1001b55f63eea54cd40d1a7963811046d526619f
Colin Beuttler <beutt102@mail.chapman.edu>
2020-09-14 15:54:30 -0700

    dkupdate1

---
+++ src/Componenets/CardForm/Card.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Cardform extends Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Technologies Used on This Site</h2>
+                <a href="http://pixelcog.github.io/parallax.js/">Parallax</a>
+
+                <p >A jQuery effect that is used to scroll either images or animations as the user scrolls.
+                I have used parallax in the past and have always been impressed with the difference it an make on my
+                wesites. It adds motion to otherwise lifeless pictures.
+            </p>
+            </div>
+        )
+    }
+}
+
+export default Cardform;
---
+++ src/Componenets/ContactForm/Contact.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import classes from './Contact.css';
+
+const contactform = () => (
+
+
+<div className={classes.Contactbox}>
+
+    <ol >
+        <li >beutt102@mail.chapman.edu </li>
+        <li >GitHub</li>
+        <li > "https://www.linkedin.com/in/colin-beuttler-5683b8149/"
+                       LinkedIn </li>
+        <li >
+            "https://docs.google.com/document/d/1hxXd6geb2dst2L_sGhxh2LuiKeHr04Q6VKzWXeCrRkg/edit"
+                        Resume</li>
+
+    </ol>
+</div>
+)
+
+
+
+
+export default contactform;
